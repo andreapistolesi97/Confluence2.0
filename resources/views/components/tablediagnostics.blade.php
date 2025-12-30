a08aa6451b2e591ef8215daa50d83fec82d44392
andreapistolesi1 <andrea.pistolesi@revelopsrl.com>
2025-12-30 10:45:14 +0100

    modifica table

--- resources/views/components/tablediagnostics.blade.php
+++ resources/views/components/tablediagnostics.blade.php
@@ -1,7 +1,7 @@
 <div class="relative overflow-y-auto max-h-[50vh] border border-gray-200 rounded-lg">
     <table class="w-full text-sm text-gray-600">
         <thead class="bg-white text-sm font-semibold whitespace-nowrap border border-color-gray-200 rounded-lg">
-            <tr>
+            <tr class="bg-white">
                 <th class="px-4 py-3 text-left font-medium ">#</th>
                 <th class="px-4 py-3 text-left">ID Contact</th>
                 <th class="px-4 py-3 text-left">Email</th>
